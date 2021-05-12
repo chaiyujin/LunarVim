c3b4dfe5a77239f80efcfdd47aad10b402047408
Yuki-Chai <chaiyujin@gmail.com>
2021-05-12 22:02:19 +0800

    nvim-tree ignore

--- lua/lv-nvimtree/init.lua
+++ lua/lv-nvimtree/init.lua
@@ -21,7 +21,12 @@ vim.g.nvim_tree_indent_markers = 1 -- "0 by default, this option shows indent ma
 vim.g.nvim_tree_follow = 1 -- "0 by default, this option allows the cursor to be updated when entering a buffer
 vim.g.nvim_tree_auto_open = 1 -- 0 by default, opens the tree when typing `vim $DIR` or `vim`
 vim.g.nvim_tree_auto_close = O.auto_close_tree -- 0 by default, closes the tree when it's the last window
-vim.g.nvim_tree_auto_ignore_ft = { 'startify', 'dashboard' } -- "empty by default, don't auto open tree on specific filetypes.
+vim.g.nvim_tree_ignore = {
+    '__pycache__'
+}
+vim.g.nvim_tree_auto_ignore_ft = {
+    'startify', 'dashboard',
+} -- "empty by default, don't auto open tree on specific filetypes.
 
 local tree_cb = require'nvim-tree.config'.nvim_tree_callback
     vim.g.nvim_tree_bindings = {
